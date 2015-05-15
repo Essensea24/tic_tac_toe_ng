93a8a70ce570ce3b3a0fc0a3db57df5759964dbd
Sophie Chor <sokpheakc@yaho.com>
2015-05-15 10:31:23 -0700

    update the array to add two object select and player key in there

--- js/TicTacToeController.js
+++ js/TicTacToeController.js
@@ -8,26 +8,18 @@ angular
  function TicTacToeController($firebaseArray) {
  	var self = this;
  	self.boxClick= boxClick;
- 	self.player1 = "x";
- 	self.player2 = "o";
- 	self.turn = "o";
  	self.addBox = getBox();
  	self.getWinner = getWinner;
+ 	
+
  	//$loaded is an event listener that only run the loop when firebase data is loaded. 
  	self.addBox.$loaded( function () {
-
  		//set a for loop to add boxes if the number of box is less than 9, then stop at 9 boxes
  		if (self.addBox.length <9 ){
  			for (var i=0; i<9; i++) {
  				//set addBox.select to an empty string
- 				self.addBox.$add({select: ""});
+ 				self.addBox.$add({select: "", player: "o"});//need to know
  			}
- 		// } else {
-			// for(var i =0; i<9; i++) {
-			// self.addBox[i].select = "";
-			// self.turn = "o";
-			// self.addBox.$save( );
-			// }
  		}
 
  	});
@@ -44,35 +36,24 @@ angular
  	function clearBoard(){
  		for(var i =0; i<9; i++) {
  			self.addBox[i].select = "";
- 			self.turn = "o";
+ 			self.addBox[i].player = "o";
  			self.addBox.$save(i);
  		}
  
  	}
 
-
  	function boxClick(e) {
- 		if (self.addBox[e].select== "" && self.turn == "o") {
- 			self.addBox[e].select = self.player1;
- 			console.log(self.addBox[e].select);
- 			self.turn = "x";
- 			console.log("next is " + self.turn + " turn");
- 			self.addBox.$save(e);
- 		} else if (self.addBox[e].select== "" && self.turn == "x") {
- 			self.addBox[e].select = self.player2;
- 			console.log(self.addBox[e].select);
- 			self.turn = "o";
- 			console.log("next is " + self.turn + " turn");
- 			self.addBox.$save(e);
- 		} else {
- 			null
+ 		if (self.addBox[e].select == "") {
+ 			if (self.addBox[e].player == "o") {
+ 				self.addBox[e].select = "x";
+ 				self.addBox[e].player = "x";//how to change the value of player for all box to "x"?
+ 				self.addBox.$save(e); // if that is impossible then try inject an firebaseObject into tttcontroler
+ 			}
  		}
- 		getWinner();
  	}
 
  	
  	function getWinner() {
- 		console.log("winner function is running");
  		//horizontal check
  		if ((self.addBox[0].select ==="x" && self.addBox[1].select ==="x" && self.addBox[2].select ==="x" ) || (self.addBox[0].select ==="o" && self.addBox[1].select ==="o" && self.addBox[2].select ==="o" )){
  				console.log(self.addBox[0].select + " 1won") 
