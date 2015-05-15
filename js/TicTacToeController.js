7e877fbc13efaaf533e087b068b558e3ee88f8e9
Sophie Chor <sokpheakc@yaho.com>
2015-05-15 15:29:06 -0700

    added working firebase including winner logic, clearboard and reset score function

--- js/TicTacToeController.js
+++ js/TicTacToeController.js
@@ -2,90 +2,112 @@ angular
     .module("tictactoeApp")
     .controller("TicTacToeController", TicTacToeController);
 
- TicTacToeController.$inject = ['$firebaseArray'];
-
+ TicTacToeController.$inject = ['$firebaseArray']
 
  function TicTacToeController($firebaseArray) {
  	var self = this;
- 	self.boxClick= boxClick;
- 	self.addBox = getBox();
+ 	self.grid = getBox();
+ 	self.boxClick = boxClick;
+ 	self.game = getTurn();
+ 	self.clearBoard = clearBoard;
  	self.getWinner = getWinner;
+ 	self.scoreReset = scoreReset;
+
  	
 
  	//$loaded is an event listener that only run the loop when firebase data is loaded. 
- 	self.addBox.$loaded( function () {
- 		//set a for loop to add boxes if the number of box is less than 9, then stop at 9 boxes
- 		if (self.addBox.length <9 ){
- 			for (var i=0; i<9; i++) {
- 				//set addBox.select to an empty string
- 				self.addBox.$add({select: "", player: "o"});//need to know
- 			}
- 		}
-
- 	});
-
+ 
 
  	function getBox() {
- 		var ref = new Firebase("https://dailytictactoe.firebaseio.com/");
- 		var box = $firebaseArray(ref);
- 		return box 
+ 		var ref = new Firebase("https://dailytictactoe.firebaseio.com/Grid");
+ 		var grid = $firebaseArray(ref);
+ 		return grid;
  	}
 
- 	self.clearBoard = clearBoard;
- 	//set select value to ""
- 	function clearBoard(){
+ 	function getTurn(){
+ 		var ref = new Firebase("https://dailytictactoe.firebaseio.com/Game");
+ 		var game = $firebaseArray(ref);
+ 		return game;
+
+ 	}
+
+ 	function clearBoard() {
+ 			self.game[0].turn = 0
+ 			self.game.$save(self.game[0]);
  		for(var i =0; i<9; i++) {
- 			self.addBox[i].select = "";
- 			self.addBox[i].player = "o";
- 			self.addBox.$save(i);
- 		}
- 
- 	}
-
- 	function boxClick(e) {
- 		if (self.addBox[e].select == "") {
- 			if (self.addBox[e].player == "o") {
- 				self.addBox[e].select = "x";
- 				self.addBox[e].player = "x";//how to change the value of player for all box to "x"?
- 				self.addBox.$save(e); // if that is impossible then try inject an firebaseObject into tttcontroler
- 			}
- 		}
- 	}
-
- 	
- 	function getWinner() {
- 		//horizontal check
- 		if ((self.addBox[0].select ==="x" && self.addBox[1].select ==="x" && self.addBox[2].select ==="x" ) || (self.addBox[0].select ==="o" && self.addBox[1].select ==="o" && self.addBox[2].select ==="o" )){
- 				console.log(self.addBox[0].select + " 1won") 
-
- 		} else if ((self.addBox[3].select ==="x" && self.addBox[4].select ==="x" && self.addBox[5].select ==="x" ) || (self.addBox[3].select ==="o" && self.addBox[4].select ==="o" && self.addBox[5].select ==="o" )){
- 				console.log(self.addBox[3].select + " 2won")
- 		}else if ((self.addBox[6].select ==="x" && self.addBox[7].select ==="x" && self.addBox[8].select ==="x" ) || (self.addBox[6].select ==="o" && self.addBox[7].select ==="o" && self.addBox[8].select ==="o" )){
- 				console.log(self.addBox[6].select + " 3won")
- 			//vertical check
- 		} else if ((self.addBox[0].select ==="x" && self.addBox[3].select ==="x" && self.addBox[6].select ==="x" ) || (self.addBox[0].select ==="o" && self.addBox[3].select ==="o" && self.addBox[6].select ==="o" )){
- 				console.log(self.addBox[0].select + " 4won")
- 		} else if ((self.addBox[1].select ==="x" && self.addBox[4].select ==="x" && self.addBox[7].select ==="x" ) || (self.addBox[1].select ==="o" && self.addBox[4].select ==="o" && self.addBox[7].select ==="o" )){
- 				console.log(self.addBox[1].select + " 5won")
- 		} else if ((self.addBox[2].select ==="x" && self.addBox[5].select ==="x" && self.addBox[8].select ==="x" ) || (self.addBox[2].select ==="o" && self.addBox[5].select ==="o" && self.addBox[8].select ==="o" )){
- 				console.log(self.addBox[2].select + " 6won")
- 			//diagonal check
- 		} else if ((self.addBox[0].select ==="x" && self.addBox[4].select ==="x" && self.addBox[8].select ==="x" ) || (self.addBox[0].select ==="o" && self.addBox[4].select ==="o" && self.addBox[8].select ==="o" )){
- 				console.log(self.addBox[0].select + " 7won")
- 		} else if ((self.addBox[2].select ==="x" && self.addBox[4].select ==="x" && self.addBox[6].select ==="x" ) || (self.addBox[2].select ==="o" && self.addBox[4].select ==="o" && self.addBox[6].select ==="o" )){
- 				console.log(self.addBox[2].select + " 8won")
- 		} else if (self.addBox[0].select == "" || self.addBox[1].select =="" ||
-				self.addBox[2].select == "" || self.addBox[3].select =="" ||
-				self.addBox[4].select == "" || self.addBox[5].select =="" ||
-				self.addBox[6].select == "" || self.addBox[7].select =="" ||
-				self.addBox[8].select == "") {
- 			console.log("keep going");
- 		} else {
- 			console.log("it's a tie")
- 			
+ 			self.grid[i].select = "empty";
+ 			self.grid.$save(i);
 		}
-
  	}
- 		
+
+ 	function scoreReset() {
+ 		self.game[0].turn = 0
+ 		self.game.$save(self.game[0]);
+ 		for(var i =0; i<9; i++) {
+ 			self.grid[i].select = "empty";
+ 			self.grid.$save(i);
+ 		}
+ 	}
+
+
+ 	function boxClick(i) {
+ 		if (self.grid[i].select == 'empty' 
+ 			&& self.game[0].turn % 2 == 1) {
+	 			self.grid[i].select = 'x';
+	 			self.game[0].turn++;
+	 			self.grid.$save(i)
+				self.game.$save(self.game[0]);
+				
+ 		} else if (self.grid[i].select == 'empty' 
+ 			&& self.game[0].turn % 2 == 0) {
+ 				self.grid[i].select = 'o';
+ 				self.game[0].turn++;
+ 				self.grid.$save(i)
+				self.game.$save(self.game[0]);
+ 		} else {
+ 			null
+ 		}
+		getWinner();
+ 	}
+
+ 	function getWinner() {
+ 		var tokens = ["x","o"]
+ 		var winners = [
+ 			[0, 1, 2],
+ 			[3, 4, 5],
+ 			[5, 7, 8],
+ 			[0, 3, 6],
+ 			[1, 4, 7],
+ 			[2, 5, 8],
+ 			[0, 4, 8],
+ 			[2, 4, 6]	
+ 		]
+
+ 		for(var i = 0; i<tokens.length; i++) {
+ 			var t = tokens[i];
+ 				for (var j = 0; j < winners.length ; j++) {
+ 					var w = winners[j]; //winner j row
+ 					if (self.grid[ w[0] ].select === t && self.grid[ w[1] ].select === t && self.grid[ w[2] ].select==t) {
+ 						console.log(t + "won")
+ 						if (t = "x") {
+ 							self.game[0].player1Score++;
+ 							self.game.$save(self.game[0]);
+ 						} else {
+ 							self.game[0].player2Score++;
+ 							self.game.$save(self.game[0]);
+ 						} 
+ 					}else if (self.grid[ w[0] ].select != t || self.grid[ w[1] ].select != t || self.grid[ w[2] ] != t) {
+ 						console.log("next player please!");
+ 					} else {
+ 						console.log("its a tie");
+ 					}
+ 				}
+ 		}
+ 	}
+
+
 }
 
+
+
+ 	
